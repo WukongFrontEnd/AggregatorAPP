479be2608c5e8dcbae86feda78b3947a1bea1a47
赵 华刚 <zhaohuagang@lifang.com>
2017-03-17 18:27:03 +0800

    aggregatorJs

--- src/application/configs/api.js
+++ src/application/configs/api.js
@@ -5,6 +5,12 @@
 4. 备注：由于很多应用只是把数据接口当做model层，而不是直接接触数据库，本文件提供数据接口配置
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------------++*/
 export default {
+    "encoding" : "utf-8" ,
+    "contentType" : "application/json" ,
+    "json" : true ,
+    "timeout" : 60 * 1000 ,  //超时请求时间，单位：毫秒
+    "successCode" : 1 ,  //restfulAPI返回的状态码status多少代表成功
+    "sessionExpireCode" : 1502 ,   //restfulAPI返回的状态码status多少代表session失效   
     "prefix" : {
         "dev" : "http://dev.hr.wkzf.com" ,
         "test" : "http://test.hr.wkzf.com" ,
@@ -12,7 +18,7 @@ export default {
         "prod" : "http://hr.wkzf.com" 
     } ,
     "suffix" : { //后缀代表接口去掉prefix的部分，这里可以是无限级的树状结构，根据自己的需要
-        "agent" : {
+        "example" : {
 
         }
     }
---
+++ src/application/mappers/example.js
@@ -0,0 +1,42 @@
+/*++----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+1. 项目名称：AggregatorAPP (一个典型的Aggregator应用系统)
+2. 文件名：src -> application -> mapper -> example.js
+3. 作者：zhaohuagang@lifang.com
+4. 备注：一个mapper的范例
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------++*/
+
+/*++----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+加载相关资源
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------++*/
+import objectMapper from "object-mapper" ;
+import logger from "../../system/libraries/logger" ;
+
+let detailMap = {
+    status : ["status", {
+        key : "code" ,
+        transform : function(value){
+            let val = parseInt(value, 10) ;
+            if(val === 1) return 200 ;
+            else return 1001 ;
+        }
+    }] ,
+    message : "msg",
+    count : "count"
+} ;
+
+class ExampleMapper {
+    constructor() {
+    }
+
+    static detail(src) {
+        try {
+            return objectMapper(src, detailMap);
+        } 
+        catch (error) {
+            logger.error(`Example Mapper==>detail:${error},src:${JSON.stringify(src)}`);
+            return null;
+        }
+    }
+}
+
+export default ExampleMapper ;
